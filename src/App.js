62c8cad2dc98d4fa60db5ba016e7550fe55d0b17
ANGELOANTU7 <angeloantu@gmail.com>
2023-06-26 15:17:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
-function App() {
+ function App() {
   return (
     <div className="App">
       <NavBar />
