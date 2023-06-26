2c3afbef8d7e63a846838418189d832bb66c7fba
ANGELOANTU7 <angeloantu@gmail.com>
2023-06-26 10:42:12 +0530

    delete

--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 3, 24, 0)}); // year, month, date, hour
-        });
-});		
-
